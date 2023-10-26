f419e95ddcf6f5ff13b8cfe696d6810cc337fb89
namrataz <40589235+namrataz@users.noreply.github.com>
2023-10-26 09:36:12 +0530

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,8 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
 
     api: {
-        key: '1a91545bacade996d1c45dc91044cbb6',
+        //key: '1a91545bacade996d1c45dc91044cbb6',
+         key: '6440392d5e93461c956f2cebb78a0767',
         baseUrl: 'https://v3.football.api-sports.io/'
     },
     resources: {
